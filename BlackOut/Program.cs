a408f0110bf04df92772c886302ce9755f67bf27
Morten <ebsen.morten@gmail.com>
2023-11-13 20:40:02 +0100

    Add project files.

---
+++ BlackOut/Program.cs
@@ -0,0 +1,36 @@
+﻿using BlackOut;
+
+if (!args.Any())
+    return;
+
+if (args[0].ToLower() == "off")
+    ScreenController.TurnOff();
+
+if (args[0].ToLower() == "on")
+    ScreenController.TurnOn();
+
+
+
+//// Opret lister over ugedage og tidspunkter
+//List<string> ugedage = new List<string> { "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag" };
+//Dictionary<string, List<TimeSpan>> tidsplan = new Dictionary<string, List<TimeSpan>>();
+
+//// Tilføj tidspunkter for hver dag
+//tidsplan["mandag"] = new List<TimeSpan> { new TimeSpan(8, 0, 0), new TimeSpan(14, 0, 0) };
+//// Gentag dette for de andre dage i ugen
+
+//// I din timer-håndteringskode
+//string aktuelDag = DateTime.Now.DayOfWeek.ToString().ToLower();
+//TimeSpan aktuelTid = DateTime.Now.TimeOfDay;
+
+//if (tidsplan.ContainsKey(aktuelDag))
+//{
+//    foreach (TimeSpan tidspunkt in tidsplan[aktuelDag])
+//    {
+//        if (aktuelTid >= tidspunkt && aktuelTid < tidspunkt.Add(new TimeSpan(0, 1, 0))) // Tjek om det er tid til at slukke eller tænde
+//        {
+//            // Brug Windows API til at slukke eller tænde skærmen
+//        }
+//    }
+//}
+
---
+++ BlackOut/ScreenController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+
+namespace BlackOut
+{
+    public static class ScreenController
+    {
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(Int32 dwFlags, Int32 dx, Int32 dy, Int32 dwData, UIntPtr dwExtraInfo);
+        
+        const int SC_MONITORPOWER = 0xF170;
+        const int WM_SYSCOMMAND = 0x112;
+        const int MONITOR_OFF = 2;
+        const int MONITOR_ON = -1;
+        const int MONITOR_Standby = 1;
+        const int MOUSEEVENTF_MOVE = 0x0001;
+
+        // Sluk skærmen
+        public static void TurnOff() => _ = SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
+
+        // Tænd skærmen
+        public static void TurnOn()
+        {
+            _ = SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_ON);
+            Wake();
+        }
+
+        private static void Wake()
+        {
+            mouse_event(MOUSEEVENTF_MOVE, 0, 1, 0, UIntPtr.Zero);
+            Thread.Sleep(40);
+            mouse_event(MOUSEEVENTF_MOVE, 0, -1, 0, UIntPtr.Zero);
+        }
+
+    }
+}
